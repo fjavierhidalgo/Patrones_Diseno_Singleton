18620b598b5953f3086dca36e158f82d0766d3e1
Francisco Javier Hidalgo <fjavier.hidalgo@gmail.com>
2019-11-19 15:45:55 +0100

    Primera versión del ejercicio

---
+++ Singleton/CentroRecogida.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class CentroRecogida
+    {
+
+        private static CentroRecogida instancia = new CentroRecogida();
+        private int tempToledo = 0;
+        private int tempMadrid = 0;
+
+        // El constructor privado evita que se instancie esta clase fuera de la misma
+        private CentroRecogida() { }
+
+        public static CentroRecogida getInstance()
+        {
+            return instancia;
+        }
+
+        public int getTempToledo()
+        {
+            return this.tempToledo;
+        }
+
+        public int getTempMadrid()
+        {
+            return this.tempMadrid;
+        }
+
+        public void setTempToledo(int grados)
+        {
+            this.tempToledo = grados;
+        }
+
+        public void setTempMadrid(int grados)
+        {
+            this.tempMadrid = grados;
+        }
+
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CentroRecogida sedeprinc_emisor1 = CentroRecogida.getInstance();
+            CentroRecogida sedeprinc_emisor2 = CentroRecogida.getInstance();
+
+            int grados = 12;
+            sedeprinc_emisor1.setTempToledo(grados);
+            Console.WriteLine(sedeprinc_emisor1.getTempToledo());
+
+            grados = 15;
+            sedeprinc_emisor2.setTempMadrid(grados);
+            Console.WriteLine(sedeprinc_emisor2.getTempToledo());
+            Console.WriteLine(sedeprinc_emisor2.getTempMadrid());
+
+        }
+    }
+}
